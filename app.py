de3c45bd9c585466434fe2e1905883d8357aa30e
aditya-g18 <anshuaditya230@gmail.com>
2024-11-09 21:15:04 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello, World! This is a CI/CD pipeline project with GitLab, Docker, and GitHub."
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
